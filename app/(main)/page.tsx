a3a89b96678851cca9c1ba9601365c85a437a569
Quỳnh Phạm Thị Ngọc <quynh.pham@namlongsoft.net>
2024-04-24 21:11:58 +0700

    update dashboard

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import { ChartBar } from './(dashboard)/ChartBar'
+import { BarChart } from 'lucide-react'
 
 const Dashboard = () => {
   return (
     <div className='body-main'>
+      <div className="flex justify-center items-center w-full gap-2 my-2">
+        <BarChart strokeWidth={2.5} size={30} className="text-primary" />
+        <h1 className="text-primary font-bold text-xl">Dashboard</h1>
+      </div>
       <ChartBar />
     </div>
   )
